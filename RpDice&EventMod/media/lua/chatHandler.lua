4ac0ffe76d4a3d4c7eeb059426f77443d38fd65c
FP2003 <filippieleckii@gmail.com>
2024-05-07 22:21:51 +0100

    Testing other input

--- RpDice&EventMod/media/lua/chatHandler.lua
+++ RpDice&EventMod/media/lua/chatHandler.lua
@@ -2,12 +2,14 @@
 -- This function will return a basic output when the player inputs !AC or alike -- 
 
 function onChatMessage(player, message)
-    local lowercaseMessage = message:lower()
-    if lowercaseMessage == "!ac" then
+    -- Check if the message starts with "/ac" (case-insensitive)
+    if message:lower():sub(1, 3) == "/ac" then
+        -- Send "Hello" to the chat
         player:Say("Hello")
     end
 end
 
+-- Register the callback function to handle chat messages
 Events.OnPlayerChat.Add(onChatMessage)
 
 
